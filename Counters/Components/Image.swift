da3478e0d524cdbe995c92e30e27fc02b4f870b8
Thalles Araújo <thalleshnaraujo@brq.com>
2023-03-23 22:41:05 -0300

    Refactor: melhora implementação de célula padrão

---
+++ Counters/Components/Image.swift
@@ -0,0 +1,8 @@
+//
+//  Image.swift
+//  Counters
+//
+//  Created by Thalles Araújo on 23/03/23.
+//
+
+import Foundation
---
+++ Counters/Helpers/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+Extension.swift
+//  Counters
+//
+//  Created by Thalles Araújo on 23/03/23.
+//
+
+import Foundation
